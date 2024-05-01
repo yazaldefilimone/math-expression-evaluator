d9b0513c7365256e3ff1e8e06528333c288cebae
Yazalde Filimone <yazaldefilimon@gmail.com>
2024-05-02 00:29:00 +0200

    *

--- core.cpp
+++ core.cpp
@@ -1,139 +1,99 @@
-#include "iostream"
-#include "core.h"
-#include <cctype>
-#include <ratio>
-// #include "vector"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
 
 using namespace std;
 
-
-// math expression evaluator
-
-
-void Tokenizer::advance(){
-    while (courser <= expression.size()){
-        switch (expression[courser]){
-            case ' ':
-                courser++;
-                break;
-            case '+':
-                tokens.push_back(Token(TokenEnum::PLUS, "+"));
-                courser++;
-                break;
-            case '-':
-                tokens.push_back(Token(TokenEnum::MINUS, "-"));
-                courser++;
-                break;
-            case '*':
-                tokens.push_back(Token(TokenEnum::MULTIPLY, "*"));
-                courser++;
-                break;
-            case '/':
-                tokens.push_back(Token(TokenEnum::DIVIDE, "/"));
-                courser++;
-                break;
-            case '(':
-                tokens.push_back(Token(TokenEnum::LPAREN, "("));
-                courser++;
-                break;
-            case ')':
-                tokens.push_back(Token(TokenEnum::RPAREN, ")"));
-                courser++;
-                break;
-            case '[':
-                tokens.push_back(Token(TokenEnum::LBRACKET, "["));
-                courser++;
-                break;
-            case ']':
-                tokens.push_back(Token(TokenEnum::RBRACKET, "]"));
-                courser++;
-                break;
-            default:
-                if (isdigit(expression[courser])){
-                    string number;
-                    while (isdigit(expression[courser])){
-                        number += expression[courser];
-                        courser++;
-                    }
-                    tokens.push_back(Token(TokenEnum::NUMBER, number));
-                } else if  (isalpha(expression[courser])){
-                    char var = expression[courser];
-                    courser++;
-                    if (isalpha(expression[courser])){
-                        cout << "Error: only one letter variables are supported" << endl;
-                        exit(1);
-                    }
-                    tokens.push_back(Token(TokenEnum::SYMBOL, string(1, var)));
-                } else {
-                    cout << "Error: invalid character" << endl;
-                    exit(1);
-                }
-
-        }
-        tokens.push_back(Token(TokenEnum::EOL, ""));
+ 
+int get_math_operation_priority(char operation) {
+    switch (operation) {
+        case '+':
+        case '-':
+        return 1;
+        case '*':
+        case '/':
+        return 2;
+        case '^':
+        return 3;
+        default:
+        return 0;
     }
 }
 
-
-Program parser(Tokenizer tokenizer){
-    Program program;
-    tokenizer.advance();
-    int index = 0;
-    do {
-        if (tokenizer.tokens[index].type == TokenEnum::NUMBER){
-            UnaryExpression unary;
-            unary.operand = tokenizer.tokens[index];
-            program.expressions.push_back(unary);
-        } else if (tokenizer.tokens[index].type == TokenEnum::SYMBOL){
-            UnaryExpression unary;
-            unary.operand = tokenizer.tokens[index];
-            program.expressions.push_back(unary);
-        } else if (tokenizer.tokens[index].type == TokenEnum::PLUS){
-            BinaryExpression binary;
-            binary._operator = tokenizer.tokens[index];
-            binary.left = tokenizer.tokens[index - 1];
-            binary.right = tokenizer.tokens[index + 1];
-            program.expressions.push_back(binary);
-        } else if (tokenizer.tokens[index].type == TokenEnum::MINUS){
-            BinaryExpression binary;
-            binary._operator = tokenizer.tokens[index];
-            binary.left = tokenizer.tokens[index - 1];
-            binary.right = tokenizer.tokens[index + 1];
-            program.expressions.push_back(binary);
-        } else if (tokenizer.tokens[index].type == TokenEnum::MULTIPLY){
-            BinaryExpression binary;
-            binary._operator = tokenizer.tokens[index];
-            binary.left = tokenizer.tokens[index - 1];
-            binary.right = tokenizer.tokens[index + 1];
-            program.expressions.push_back(binary);
-        } else if (tokenizer.tokens[index].type == TokenEnum::DIVIDE){
-            BinaryExpression binary;
-            binary._operator = tokenizer.tokens[index];
-            binary.left = tokenizer.tokens[index - 1];
-            binary.right = tokenizer.tokens[index + 1];
-            program.expressions.push_back(binary);
-        }
-        index++;
-    } while (tokenizer.tokens[index].type != TokenEnum::EOL);
-    return program;
+bool is_operator(char operation) {
+    return operation == '+' || operation == '-' || operation == '*' || operation == '/' || operation == '^';
+}
+bool is_close_parenthesis(char operation) {
+  return operation == ')';
+}
+bool is_open_parenthesis(char operation) {
+  return operation == '(';
+}
+int string_to_int(string str_number) {
+    // 10_000_000 -> 10000000
+    string _number = "";
+    //!todo: use internal function to remove all '_' characters
+    for (int cursor = 0; cursor < str_number.size(); cursor++) {
+      if (str_number[cursor] != '_') {
+        _number += str_number[cursor];
+      }
+    }
+    return stoi(_number);
 }
 
+bool is_digit(char character) {
+    return isdigit(character) || character == '_';
+}
+bool is_whitespace(char character) {
+    return character == ' ';
+}
 
+int binary_operation(int left_operand, int right_operand, char operation) {
+  switch (operation) {
+    case '+':
+      return left_operand + right_operand;
+    case '-':
+      return left_operand - right_operand;
+    case '*':
+      return left_operand * right_operand;
+    case '/':
+      return left_operand / right_operand;
+    case '^':
+      return pow(left_operand, right_operand);
+    default:
+      return 0;
+  }
+}
+int evaluate(string expression) {
+  vector<int> stack;
+  for (int cursor =  0; cursor < expression.size(); cursor++) {
+    if (expression[cursor] == ' ') {
+      continue;
+    }
+    if (isdigit(expression[cursor])) {
+        string str_num = "";
+        while (cursor < expression.size() && is_digit(expression[cursor])) {
+          str_num += expression[cursor];
+          cursor++;
+        }
+        int number = string_to_int(str_num);
+        stack.push_back(number);
+    }
+    if (is_operator(expression[cursor]) && stack.size() >= 2) {
+      int right_operand = stack.back();
+      stack.pop_back();
+      int left_operand = stack.back();
+      stack.pop_back();
+      int result = binary_operation(left_operand, right_operand, expression[cursor]);
+      stack.push_back(result);
+    }
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return stack.back();
+}
 
 int main() {
-  return 0;
+    string expression = "10 + 2 * 6";
+    cout << evaluate(expression) << endl;
 }
